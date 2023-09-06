31c66c64e27588370662a9fac90fa18a15e7167b
Jeff Hitchcock <mail@command-center.org>
2023-09-05 21:04:27 -0700

    [#1] Set up project structure

---
+++ black-flag.mjs
@@ -0,0 +1,30 @@
+/**
+ * The Black Flag Roleplaying game system for Foundry Virtual Tabletop.
+ * Software License: MIT
+ * Repository: https://github.com/koboldpress/black-flag
+ * Issue Tracker: https://github.com/koboldpress/black-flag/issues
+ */
+
+globalThis.BlackFlag = {
+	modules: {}
+};
+
+Hooks.once("init", function() {
+	
+});
+
+Hooks.once("setup", function() {
+	
+});
+
+Hooks.once("i18nInit", function() {
+	
+});
+
+Hooks.once("ready", function() {
+	
+});
+
+export {
+
+};
