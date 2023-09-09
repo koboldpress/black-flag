2ec88f0e306e1feab1b0f6a0d7ae5ccbff1bef05
Jeff Hitchcock <mail@command-center.org>
2023-09-09 09:28:42 -0700

    [#2] Move all code into folder, adjust rollup to also build styles

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+import postcssImport from "postcss-import";
+import postcssPresetEnv from "postcss-preset-env";
+
+export default {
+	plugins: [
+		postcssImport,
+		postcssPresetEnv
+	]
+};
---
+++ rollup.config.mjs
@@ -0,0 +1,16 @@
+import path from "path";
+import postcss from "rollup-plugin-postcss";
+
+export default {
+	input: "code/_module.mjs",
+	output: {
+		file: "black-flag.mjs",
+		format: "es",
+		sourcemap: true
+	},
+	plugins: [
+		postcss({
+			extract: path.resolve("black-flag.css")
+		})
+	]
+};
