f588bb830c320132b4ea60816657fbdef6ac87f6
Jeff Hitchcock <mail@command-center.org>
2023-09-09 14:34:21 -0700

    [#3] Implement initial PC sheet layout

--- code/applications/actor/pc-sheet.mjs
+++ code/applications/actor/pc-sheet.mjs
@@ -5,6 +5,9 @@ export default class PCSheet extends ActorSheet {
 			classes: ["black-flag", "actor", "sheet", "pc"],
 			width: 820,
 			height: 740,
+			tabs: [
+				{navSelector: 'nav[data-group="primary"]', contentSelector: "main", initial: "main"}
+			],
 			template: "systems/black-flag/templates/actor/pc.hbs"
 		});
 	}
