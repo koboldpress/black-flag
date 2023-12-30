a2ea8614bc19377d3a539307191a395a681e1a0f
Jeff Hitchcock <mail@command-center.org>
2023-12-30 11:11:40 -0800

    [#303] Re-organize compendium sources into folders

--- utils/package/unpack.mjs
+++ utils/package/unpack.mjs
@@ -25,16 +25,54 @@ export default async function unpackDB(packName, entryName, options) {
 
 	for ( const packInfo of packs ) {
 		const dest = Path.join(PACK_SRC, packInfo.name);
+
+		// Determine folders for extracting
+		const folders = {};
+		const containers = {};
+		await extractPack(packInfo.path, dest, {
+			log: false, transformEntry: e => {
+				if ( e._key.startsWith("!folders") ) folders[e._id] = { name: slugify(e.name), folder: e.folder };
+				else if ( e.type === "container" ) containers[e._id] = {
+					name: slugify(e.name), container: e.system?.container, folder: e.folder
+				};
+				return false;
+			}
+		});
+		const buildPath = (collection, entry, parentKey) => {
+			let parent = collection[entry[parentKey]];
+			entry.path = entry.name;
+			while ( parent ) {
+				entry.path = Path.join(parent.name, entry.path);
+				parent = collection[parent[parentKey]];
+			}
+		};
+		Object.values(folders).forEach(f => buildPath(folders, f, "folder"));
+		Object.values(containers).forEach(c => {
+			buildPath(containers, c, "container");
+			const folder = folders[c.folder];
+			if ( folder ) c.path = Path.join(folder.path, c.path);
+		});
+
 		await extractPack(packInfo.path, dest, {
 			log: true, documentType: packInfo.type, transformEntry: entry => {
 				if ( entryName && (entryName !== entry.name.toLowerCase()) ) return false;
 				cleanPackEntry(entry);
 			}, transformName: entry => {
-				const name = entry.name.toLowerCase();
-				const outputName = name.replace("'", "").replace(/[^a-z0-9]+/gi, " ").trim().replace(/\s+|-{2,}/g, "-");
-				// const subfolder = _getSubfolderName(entry, packInfo.name);
-				return Path.join(/*subfolder*/"", `${outputName}.${entry._id}.json`);
+				if ( entry._id in folders ) return Path.join(folders[entry._id].path, "_folder.json");
+				if ( entry._id in containers ) return Path.join(containers[entry._id].path, "_container.json");
+				const outputName = slugify(entry.name);
+				const parent = containers[entry.system?.container] ?? folders[entry.folder];
+				return Path.join(parent?.path ?? "", `${outputName}.${entry._id}.json`);
 			}
 		});
 	}
+}
+
+/**
+ * Standardize name format.
+ * @param {string} name
+ * @returns {string}
+ */
+function slugify(name) {
+	return name.toLowerCase().replace("'", "").replace(/[^a-z0-9]+/gi, " ").trim().replace(/\s+|-{2,}/g, "-");
 }
