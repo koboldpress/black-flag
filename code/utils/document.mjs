76dfbdfcf9ead93a627979ffb11c7f66d098c4e6
Jeff Hitchcock <mail@command-center.org>
2024-04-21 15:08:25 -0700

    [#462] Fix linkForUUID with element option in V12

--- code/utils/document.mjs
+++ code/utils/document.mjs
@@ -6,13 +6,12 @@
  * @returns {string|HTMLElement} - Link to the item or empty string if item wasn't found.
  */
 export function linkForUUID(uuid, { element=false }={}) {
-	if ( game.release.generation < 12 ) {
-		const result = TextEditor._createContentLink(["", "UUID", uuid]);
-		return element ? result : result.outerHTML;
-	}
+	let result;
+
+	if ( game.release.generation < 12 ) result = TextEditor._createContentLink(["", "UUID", uuid]);
 
 	// TODO: When v11 support is dropped we can make this method async and return to using TextEditor._createContentLink.
-	if ( uuid.startsWith("Compendium.") ) {
+	else if ( uuid.startsWith("Compendium.") ) {
 		let [, scope, pack, documentName, id] = uuid.split(".");
 		if ( !CONST.PRIMARY_DOCUMENT_TYPES.includes(documentName) ) id = documentName;
 		const data = {
@@ -21,7 +20,10 @@ export function linkForUUID(uuid, { element=false }={}) {
 		};
 		TextEditor._createLegacyContentLink("Compendium", [scope, pack, id].join("."), "", data);
 		data.dataset.link = "";
-		return TextEditor.createAnchor(data).outerHTML;
+		result = TextEditor.createAnchor(data);
 	}
-	return fromUuidSync(uuid).toAnchor().outerHTML;
+
+	else result = fromUuidSync(uuid).toAnchor();
+
+	return element ? result : result.outerHTML;
 }
